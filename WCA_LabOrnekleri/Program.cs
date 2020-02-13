3620b641c51677c4a8cace5cecd373b9c92582b5
muratvuranok <mrtvrnk@gmail.com>
2020-02-13 16:50:42 +0300

    Add project files.

---
+++ WCA_LabOrnekleri/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace WCA_LabOrnekleri
+{
+    class Program
+    {
+        // NOT : Consol üzerine yazılan datayı  içeriye almayı araştırınız :)
+        static void Main(string[] args)
+        {
+            // Dışarıdan nasıl data alıcaz diye düşünüyosanız, üstteki not'u okuyunuz :)
+
+
+            //1) Disaridan alinan iki sayının toplamiyla farkinin birbirine bolumunden kalanin sonucu kactir? (Farkın toplama bolumunden kalan kactir?)
+
+            //2) Disaridan girilen bir sayının 10 eksiginin 20 fazlasinin 2ye bolumunden kalaninin karesi kactir?
+
+            //3) Vize notu'nun % 30'u, final notu'nun % 70'ini alıp öğrencinin not ortalamasini cikartan bir uygulama yaziniz... DİKKAT => Notlar ondalikli olabilir?
+
+            //4) Dışarıdan kullanıcı adını ve soyadını ayrı ayrı olarak alnız ve  adi.soyadi@hotmail.com şeklinde mail adresi oluşturup kullanıcıya gösteriniz
+
+            //5) Disaridan girilen iki sayının karelerinin toplami ile karelerinin farki toplami kactir?
+        }
+    }
+}
